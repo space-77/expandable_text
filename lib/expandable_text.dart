f796f869a73b1803d212d1144e0e49d3ae957ad5
lirandong <lirandong@maxrocky.com>
2020-04-14 16:22:00 +0800

    init

---
+++ lib/expandable_text.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:expandable/expandable.dart';
+
+class ExpandableText extends StatefulWidget {
+  final String text;
+  final int maxLines;
+  final TextStyle style;
+  final TextOverflow overflow;
+  ExpandableText({
+    Key key,
+    @required this.text,
+    this.maxLines,
+    this.style,
+    this.overflow = TextOverflow.fade
+  }) : super(key: key);
+
+  @override
+  _ExpandableTextState createState() => _ExpandableTextState();
+}
+
+class _ExpandableTextState extends State<ExpandableText> {
+  int get maxLines => widget.maxLines;
+  String get text => widget.text;
+  TextStyle get style => widget.style;
+  TextOverflow get overflow => widget.overflow;
+  
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, size) {
+      final span = TextSpan(text: text, style: style);
+      final tp = TextPainter(
+        text: span, maxLines: maxLines,
+        textDirection: TextDirection.ltr
+      );
+      tp.layout(maxWidth: size.maxWidth);
+ 
+      if (tp.didExceedMaxLines) {
+        return ExpandableNotifier(
+        child: Column(
+          children: [
+            Expandable(
+              collapsed: ExpandableButton(
+                child: Column(
+                  children: [
+                    Text(text, maxLines: maxLines, overflow: overflow, style: style),
+                    ExpandableButton(
+                      child: Text('打开', style: TextStyle(color: Colors.blue)),
+                    )
+                  ]
+                ),
+              ),
+              expanded: Column(
+                children: [
+                  Text(text, style: style),
+                  ExpandableButton(
+                    child: Text('收起', style: TextStyle(color: Colors.blue)),
+                  ),
+                ]
+              ),
+            )
+          ]
+        ),
+      );
+      } else {
+        return Text(text, style: style);
+      }
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:expandable_text/expandable_text.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    String text = '''
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+      文字溢出，文字溢出,文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，文字溢出，
+    ''';
+    String text2 = '四个文字';
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          child: Column(
+            children: [
+              ExpandableText(text: text, maxLines: 5),
+              SizedBox(height: 30, width: 30),
+              ExpandableText(text: text2, maxLines: 5),
+            ]
+          )
+        )
+      ),
+    );
+  }
+}
